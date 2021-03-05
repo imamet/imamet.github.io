27566efb605f858fc563ee23725bd774ebe05dd3
Imam Slamet <imamslamet810@gmail.com>
2021-03-05 16:58:56 +0700

    Update time.js

--- js/time.js
+++ js/time.js
@@ -1,4 +1,4 @@
-let a = "16:40:00";
+let a = "17:00:00";
 let date1 = new Date();
 date1.setHours(a.split(":")[0]);
 date1.setMinutes(a.split(":")[1]);
@@ -9,7 +9,7 @@ setTimeout(function() {
     $('.kedua').addClass('show');
 }, r);
 // set time 2
-let b = "16:40:10";
+let b = "17:00:10";
 let date2 = new Date();
 date2.setHours(b.split(":")[0]);
 date2.setMinutes(b.split(":")[1]);
@@ -20,7 +20,7 @@ setTimeout(function() {
     $('.ketiga').addClass('show');
 }, s);
 // set time 3
-let c = "16:40:20";
+let c = "17:00:20";
 let date3 = new Date();
 date3.setHours(c.split(":")[0]);
 date3.setMinutes(c.split(":")[1]);
@@ -31,7 +31,7 @@ setTimeout(function() {
     $('.keempat').addClass('show');
 }, t);
 // set time 4
-let d = "16:40:30";
+let d = "17:00:30";
 let date4 = new Date();
 date4.setHours(d.split(":")[0]);
 date4.setMinutes(d.split(":")[1]);
@@ -42,7 +42,7 @@ setTimeout(function() {
     $('.kelima').addClass('show');
 }, u);
 // set time 5
-let e = "16:40:40";
+let e = "17:00:40";
 let date5 = new Date();
 date5.setHours(e.split(":")[0]);
 date5.setMinutes(e.split(":")[1]);
